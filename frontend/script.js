89a2299fa3cd5f9884bc825b8c10debf1883d7a8
Payal Rani <bharadwajpayal143@gmail.com>
2026-04-09 19:46:19 +0530

    Added UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 async function getData() {
-    const res = await fetch("http://localhost:5000/api/message");
+    const res = await fetch("/api/message");
     const data = await res.json();
     document.getElementById("result").innerText = data.message;
 }
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
+// Middleware
 app.use(cors());
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
 app.get("/api/message", (req, res) => {
     res.json({ message: "Hello from backend 🚀" });
 });
 
-const PORT = 5000;
+// Server
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
